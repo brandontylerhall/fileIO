0a49006d76baf1ff3089bf0ca3e479fda46b5d27
Brandon Hall <bran.tyler.hall@gmail.com>
2023-11-03 10:43:37 -0400

    created skeleton of manager methods

---
+++ src/ContactManager.java
@@ -0,0 +1,27 @@
+public class ContactManager {
+    boolean quit = false;
+
+    public void showContacts() {
+
+    }
+
+    public void addContact() {
+
+    }
+
+    public void searchByName() {
+
+    }
+
+    public void removeContact() {
+
+    }
+
+    public void quitFlag() {
+        boolean quit = true;
+    }
+
+    public boolean quit() {
+        return quit;
+    }
+}
