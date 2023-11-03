abae99b25d0ed07da098673e64281ac5e6fa7fb5
Brandon Hall <bran.tyler.hall@gmail.com>
2023-11-03 13:53:38 -0400

    changed phoneNumber from an int to a string so other various methods would actually work

--- src/Contact.java
+++ src/Contact.java
@@ -1,8 +1,8 @@
 public class Contact {
     private String name;
-    private int phoneNumber;
+    private String phoneNumber;
 
-    public Contact(String name, int phoneNumber) {
+    public Contact(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
@@ -15,11 +15,11 @@ public class Contact {
         this.name = name;
     }
 
-    public int getPhoneNumber() {
+    public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 }
