cecdf13666b8ce863cf516f8d09d617961804952
Brandon Hall <bran.tyler.hall@gmail.com>
2023-11-03 13:54:54 -0400

    added an IOException to the app class

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -1,26 +1,33 @@
 import util.Input;
-public class ContactsApp {
-    public static void main(String[] args) {
-        ContactManager contactManager = new ContactManager();
-        Input in = new Input();
 
-        while (!contactManager.quit()) {
-            System.out.println("Select the function number you would like to perform.");
-            System.out.printf("" +
-                    "1. Add a contact%n" +
-                    "2. Remove a contact%n" +
-                    "3. Show contact list%n" +
-                    "4. Search contact list by name%n" +
-                    "5. Close program%n" +
-                    "> ");
-            int userSelection = in.getInt();
-            switch (userSelection) {
-                case 1 -> contactManager.addContact();
-                case 2 -> contactManager.removeContact();
-                case 3 -> contactManager.showContacts();
-                case 4 -> contactManager.searchByName();
-                case 5 -> contactManager.quitFlag();
+import java.io.IOException;
+
+public class ContactsApp {
+    public static void main(String[] args) throws IOException {
+        try {
+            ContactManager contactManager = new ContactManager();
+            Input in = new Input();
+
+            while (!contactManager.quit()) {
+                System.out.println("Select the function number you would like to perform.");
+                System.out.printf("" +
+                        "1. Add a contact%n" +
+                        "2. Remove a contact%n" +
+                        "3. Show contact list%n" +
+                        "4. Search contact list by name%n" +
+                        "5. Close program%n" +
+                        "> ");
+                int userSelection = in.getInt();
+                switch (userSelection) {
+                    case 1 -> contactManager.addContact();
+                    case 2 -> contactManager.removeContact();
+                    case 3 -> contactManager.showContacts();
+                    case 4 -> contactManager.searchByName();
+                    case 5 -> contactManager.quitFlag();
+                }
             }
+        } catch (IOException e) {
+            System.out.println("Error occurred while adding contact: " + e.getMessage());
         }
     }
 }
